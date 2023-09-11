21b880b865905ba343f010c10578be2192c87f55
Khazratbek <100770676+Xazratbek@users.noreply.github.com>
2023-09-11 18:54:56 +0500

    birinchi commit

---
+++ 2-dars/print_toliq.py
@@ -0,0 +1,37 @@
+print('Assalom alaykum')
+print("Hayrli tong")
+print(2+4*2)
+print(19/5)
+print(2**4)
+print('Men "Dell" markasidagi noutbuk sotib oldim')
+print("""Odami ersang, demagil odami,
+Oniki, yo'q xalq g'amidin g'ami""")
+print("Odami ersang, demagil odami,\nOniki, yo'q xalq g'amidin g'ami")
+print(19/5)
+print('3x3=',3*3)
+print(2*5*3.14159)
+
+#Radiusi 5 ga teng bo'lgan aylananing uzunligi quyidagicha hisoblanadi
+print(2*5*3.14159)
+
+#Vazifalar
+
+# 0-vazifa **Yuqoridagi misollarni o'zingiz ishlashga harakat qiling, so'ngra quyidagi javoblar bilan solishtiring.**
+
+print("\"Nexia\", \"Tico\", 'Damas' ko'rganlar qilar havas")
+
+#1-vazifa 5 ning 4-darajasini toping
+print(f"5-ning 4-darajasi:{5**4}-ga teng")
+
+# 22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+print(f"22-ni 4-ga bo'lganda {22%4}-qoldiq qoladi")
+
+# Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+#3 Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+print("Tomonlari 125 ga teng kvadratning yuzi", 125*125, "ga, perimetri", 4*125, "teng")
+
+#4 Diametri 12 ga teng bo'lgan doiraning yuzini toping
+print('Diametri 12 ga teng bo\'lgan doiraning yuzi', 3.14*(12/2)**2, 'ga teng')
+
+#5 Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", (6**2+7**2)**(1/2))
