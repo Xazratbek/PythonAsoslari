d4fa7d7ef1cad72d068af9a41b94be8e6dff7be6
Khazratbek <100770676+Xazratbek@users.noreply.github.com>
2023-09-12 20:37:34 +0500

    yangi dars kodlari

---
+++ lists.py
@@ -0,0 +1,122 @@
+mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
+narhlar = [12000, 18000, 10900, 22000] # narhlar ro'yxati (sonlar)
+sonlar = ['bir', 'ikki', 3, 4, 5] # sonlar va matnlar aralash ro'yxat
+ismlar = [] # bo'sh ro'yxat
+
+mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
+print("Birinchi meva: ", mevalar[0])
+print("Ikkinchi meva: ", mevalar[1])
+
+mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
+print("Birinchi meva: ", mevalar[0].title())
+print("Ikkinchi meva: ", mevalar[1].upper())
+
+narhlar = [12000, 18000, 10900, 22000]
+print(narhlar[2] + narhlar[3])
+
+
+car_models = ['Toyota', 'GM', 'Volvo', 'BMW', 'Hyundai', 'Kia', 'Volkswagen']
+print(car_models[-1]) # Listning eng oxirgi elementiga -1 bilan murojat qilamiz
+
+narhlar = [12000, 18000, 10900, 22000]
+narhlar[0] = 13000 # 1-qiymatni 13000 ga o'zgartiramiz
+narhlar[2] = 11000 # 3-qiymatni 11000 ga o'zgartiramiz
+narhlar[3] = narhlar[3]+2000 # 4-qiymatga 2000 qo'shamiz
+print(narhlar)
+
+
+mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"]
+mevalar.append("tarvuz") # mevalar ga tarvuz qo'shamiz
+print(mevalar)
+
+
+mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"]
+mevalar.append("tarvuz") # mevalar ga tarvuz qo'shamiz
+print(mevalar)
+
+
+cars = ['Lacetti', 'Nexia 3', 'Cobalt']
+cars.insert(0, 'Malibu') # 1-o'ringa yangi qiymat qo'shamiz
+print(cars)
+
+
+cars.insert(2, 'Damas') # 3-o'ringa yangi qiymat qo'shamiz
+print(cars)
+
+mevalar = ['olma', 'anjir', 'shaftoli', "o'rik", 'anor']
+del mevalar[1] # 2-element (anjir) ni o'chirib tashlaymiz
+print(mevalar)
+
+
+mevalar = ['olma', 'anjir', 'shaftoli', "o'rik", 'anor']
+mevalar.remove('shaftoli') # Ro'yxatdan shaftolini o'chirdik
+print(mevalar)
+
+hayvonlar = ['it', 'mushuk', 'sigir', 'qo\'y', 'quyon', 'mushuk']
+hayvonlar.remove("mushuk") # Ro'yxatda 2 ta mushuk bor, ulardan birinchisi o'chadi
+print(hayvonlar)
+
+bozorlik = ["yog'", 'un', 'piyoz', 'banan', "go'sht"]
+mahsulot = bozorlik.pop(3) # Ro'yxatdan banan ni sug'urib olamiz
+print("Men " + mahsulot + " sotib oldim")
+print("Olinmagan mahsulotlar: ", bozorlik)
+
+
+#Amaliyot
+
+ismlar = ["anvar","alisher","sarvar"]
+
+print(f"Salom {ismlar[-1]}, bugun dars qilamizmi ?")
+print(f"Salom {ismlar[0]}, bugun kursga boramizmi ?")
+
+
+# sonlar deb nomlangan ro'yxat yarating va ichiga turli sonlarni yuklang (musbat, manfiy, butun, o'nlik).
+sonlar = [22, -58.2, 34.0, 67, 1983, 123_456_678_000, 112.4]
+print(sonlar)
+
+# Yuqoridagi ro'yxatdagi sonlar ustida turli arifmetik amallar bajarib ko'ring. Ro'yxatdagi ba'zi sonlarning qiymatini o'zgartiring, ba'zilarini esa almashtiring.
+sonlar[0] = sonlar[0]+sonlar[-1]
+sonlar[1] = -67.8
+sonlar[4] = sonlar[4] + 37
+del sonlar[5]
+print(sonlar)
+
+#t_shaxslarva z_shaxslar degan 2 ta ro'yxat yarating va biriga o'zingiz eng ko'p hurmat qilgan tarixiy shaxslarning, ikkinchisiga esa zamonamizdagi tirik bo'lgan shaxslarning ismini kiriting.
+t_shaxslar = ['Amir Temur','Imom Buxoriy', 'Napoleon']
+z_shaxslar = ['Bill Gates', 'Elon Musk', 'Doasnald Trump']
+
+
+#Yuqoridagi ro'yxatlarning har biridan bittadan qiymatni sug'urib olib (.pop()), quyidagi ko'rinishda chiqaring:
+print(f"\nMen tarixiy shaxslardan {t_shaxslar.pop(1)} bilan,\n\
+zamonaviy shaxslardan esa {z_shaxslar.pop(0)} bilan\n\
+suhbat qilishni istar edim\n")
+
+
+#friends nomli bo'sh ro'yxat tuzing va unga .append() yordamida 5-6 ta mehmonga chaqirmoqchi bo'lgan do'stlaringizni kiriting.
+friends = []
+friends.append('John')
+friends.append('Alex')
+friends.append('Danny')
+friends.append('Sobirjon')
+friends.append('Vanya')
+print(friends)
+
+
+#Yuqoridagi ro'yxatdan mehmonga kela olmaydigan odamlarni .remove() metodi yordamida o'chrib tashlang.
+friends.remove('John')
+friends.remove('Alex')
+print(friends)
+
+#Ro'yxatning oxiriga, boshiga va o'rtasiga yangi ismlar qo'shing.
+friends.append('Hasan')
+friends.insert(0, 'Husan')
+friends.insert(2, 'Ivan')
+print(friends)
+
+
+#Yangi mehmonlar deb nomlangan bo'sh ro'yxat yarating. .pop() va .append() metodlari yordamida mehmonga kelgan do'stlaringizning ismini friends ro'yxatidan sug'urib olib, mehmonlar ro'yxatiga qo'shing.
+mehmonlar = []
+mehmonlar.append(friends.pop(3))
+mehmonlar.append(friends.pop(-1))
+mehmonlar.append(friends.pop(0))
+print("\nKelgan mehmonlar: ", mehmonlar)
